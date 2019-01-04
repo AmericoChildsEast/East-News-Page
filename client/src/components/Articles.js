ef37949a3af1abb7d1d67862284b79fccf059b18
Sadiq <sadiq4wans@gmail.com>
2019-01-04 13:12:37 -0600

    added styles on the artical container in Articles.js

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -11,20 +11,43 @@ class Articles extends Component {
                  <div>
                     <SideMenu />
                     <div className="contentBox">
-                        <h1 style={{textAlign:"center",color:""}}>
+
+                        <h1 style={{textAlign:"center",
+                                        color:""}}>
                            All articles in the data base are to be shown here 
                         </h1>
-                        <div id="textBox">
-                           <div id="innerBox"></div>
-                           <div id="a-Txt">
-                           <h4>Heading appears here</h4>
-                           <p>Sample of article text appers here</p>
-                           </div>
-                           <div id="innerBox2"> 
+
+                        <div id="articleBox">
+
+                           <div id="visualBox"  style={{textAlign:"", color:"", padding:"5px 0 0 60px", fontSize:90}}>
+                                 <p style={{textAlign:"",color:"white"}}> H </p> 
+                            </div>
+
+                            <div id="article_container" style={{border:"none", 
+                                                                 width:"60%",
+                                                                 height:"160px",
+                                                                 float:"left",
+                                                                 backgroundColor:""}}>
+                               <div id="sample_txt_Box">
+                                 <h4>Article heading appears here</h4>
+                                 <p>Sample text of article appers here...</p>
+                              </div>
+                           
+                              <span style={{textAlign:"",
+                                                float:"right",
+                                               border:"none",
+                                               margin:"6px 5px 0 0",
+                                               color:"#D5D8DC"}}>
+                                                Date:  1/4/2019
+                              </span>
+                            </div>
+                           <div id="manage_btn_Box"> 
                                <Button id="delBtn">Delete</Button>
                                <Button id="editBtn">Edit</Button>
-                           </div>  
+                           </div> 
+                           
                         </div>
+
                   </div>
                  </div>
            );
