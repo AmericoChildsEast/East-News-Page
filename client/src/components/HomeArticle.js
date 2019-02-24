16abf998e1c216a7a76099f74033fda5698a61b1
Sadiq <sadiq4wans@gmail.com>
2019-02-24 13:26:08 -0600

    still working home article cards

--- client/src/components/HomeArticle.js
+++ client/src/components/HomeArticle.js
@@ -10,9 +10,12 @@ function ArticleCard() {
     return (
         <div id="article-card">
             <div id="card-img">
-
+            {/* img / letters / colors */}
+             <h5>Title goes here and background images...</h5>
             </div>
             <div id="card-footer">
+             <p>Sample text goes here...</p>
+             <p>NOTE: this is just a temporary example</p>
             </div>
         </div>
     );
@@ -21,7 +24,7 @@ function ArticleCard() {
 class HomeArticle extends Component {
     printMoreBox() {
         let numbox = [];
-        for (var i = 1; i < 2; i++) {
+        for (var i = 1; i < 10; i++) {
 
             numbox.push(<ArticleCard />);
         }
